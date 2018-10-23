6843fffeef574137754fad839e787ac37539ad1a
Du <Sampan0103314@gmail.com>
2018-10-23 11:29:49 -0400

    deploy

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,7 @@
 import { REQUEST_RESTAURANTS_SUCCESS, REQUEST_RESTAURANTS_FAIL } from './constants';
 
 export const requestRestaurants = (city, offset) => (dispatch) => {
-  fetch(`http://localhost:3001/api/search?location=${city}&offset=${offset}`)
+  fetch(`https://sleepy-river-73437.herokuapp.com/api/search?location=${city}&offset=${offset}`)
     .then(response => response.json())
     .then(restaurants =>
       dispatch({
