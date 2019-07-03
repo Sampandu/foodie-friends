d44e3191b6980d9fddff44938c3342630264985b
Du <Sampan0103314@gmail.com>
2019-07-03 19:35:30 -0400

    connect to live server

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,7 @@
 import { REQUEST_RESTAURANTS_SUCCESS, REQUEST_RESTAURANTS_FAIL } from './constants';
 
 export const requestRestaurants = (city, offset, latitude, longitude) => (dispatch) => {
-  fetch(`http://localhost:3000/api/search?location=${city}&offset=${offset}&latitude=${latitude}&longitude=${longitude}`)
+  fetch(`https://sleepy-river-73437.herokuapp.com/api/search?location=${city}&offset=${offset}&latitude=${latitude}&longitude=${longitude}`)
     .then(response => response.json())
     .then(restaurants =>
       dispatch({
